e1833eb525ef37aff6a1ce5d24adadfc801f7146
MatthO12 <23124029@srcas.ac.in>
2025-01-25 15:21:15 +0530

    Update amstrong.java

--- amstrong.java
+++ amstrong.java
@@ -1,37 +1,26 @@
+
 import java.util.Scanner;
 
-public class ArmstrongNumber {
-
+public class ReverseNumber {
     public static void main(String[] args) {
-        // Create scanner object to read input
         Scanner scanner = new Scanner(System.in);
-
+        
+        // Input: Take the number from the user
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
-
-        // Call function to check if the number is an Armstrong number
-        if (isArmstrong(number)) {
-            System.out.println(number + " is an Armstrong number.");
-        } else {
-            System.out.println(number + " is not an Armstrong number.");
+        
+        int reversedNumber = 0;
+        
+        // Reverse the number
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number = number / 10;
         }
-
+        
+        // Output: Display the reversed number
+        System.out.println("Reversed Number: " + reversedNumber);
+        
         scanner.close();
-    }
-
-    // Function to check if a number is Armstrong number
-    public static boolean isArmstrong(int num) {
-        int originalNumber = num;
-        int sum = 0;
-        int digits = String.valueOf(num).length(); // Find number of digits
-
-        while (num > 0) {
-            int digit = num % 10; // Extract last digit
-            sum += Math.pow(digit, digits); // Raise digit to the power of number of digits and add to sum
-            num /= 10; // Remove the last digit
-        }
-
-        // Check if sum equals the original number
-        return sum == originalNumber;
     }
 }
