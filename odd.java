a88f6a00222031d47bbe4850578650c743aba559
MatthO12 <23124029@srcas.ac.in>
2025-01-25 15:04:39 +0530

    Update odd.java

--- odd.java
+++ odd.java
@@ -1,12 +1,9 @@
-public class OddNumbers {
-
-    public static void main(String[] args) {
-        // Loop through numbers from 1 to 100
-        for (int i = 1; i <= 100; i++) {
-            // Check if the number is odd
-            if (i % 2 != 0) {
-                // Print the odd number
-                System.out.println(i);
+class OddNumber {
+public static void main(String args[]) {
+System.out.println("The Odd Numbers are:");
+for (int i = 1; i <= 100; i++) {
+if (i % 2 != 0) {
+System.out.print(i + " ");
             }
         }
     }
